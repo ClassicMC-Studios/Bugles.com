09271271790c4b0dcccd2b271fdb6e3a194967c6
ClassicMC <classicmclive@gmail.com>
2022-10-04 19:02:07 +0000

    haöbiödulöag

---
+++ bugle-wiki/src/elements/body.jsx
@@ -0,0 +1,11 @@
+import './panels/w3.css'
+import './panels/bc.css';
+
+function Body(){
+    return(
+        <div className='w3-conatiner'>
+            <p>The bugle is a musical instrument from the brass family</p>
+        </div>
+    );
+};
+export default Body;
